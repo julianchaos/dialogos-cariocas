b88d66bdd838480d6118248dca5ce4682085af6c
Julian Andrade <julianchaos@gmail.com>
2015-05-03 21:12:57 -0300

    Correção do layout nas páginas internas e título para páginas internas.

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ get_header(); ?>
 					<?php while ( have_posts() ) : the_post(); ?>
 						<?php get_template_part( 'content', 'page' ); ?>
 					<?php endwhile; // end of the loop. ?>
-
+					<br class='nobr' />
 				</div>
 				<!-- #conteudoTotal -->
 			</div>
--- page.php
+++ page.php
@@ -9,10 +9,15 @@ get_header();
 ?>
 			<div id="abas-1" class="menuPrincipalTabContent">
 				<div class="conteudoTotal">
-					<?php while ( have_posts() ) : the_post(); ?>
+					<?php if(is_string(get_field('titulo_pagina'))) { ?>
+					<h2><?php the_field('titulo_pagina') ?></h2>
+					<?php
+					
+					}
+					while ( have_posts() ) : the_post(); ?>
 						<?php get_template_part( 'content', 'page' ); ?>
 					<?php endwhile; // end of the loop. ?>
-
+					<br class='nobr' />
 				</div>
 				<!-- #conteudoTotal -->
 			</div>
